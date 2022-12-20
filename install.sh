a3d9311e446175f12eac0edceee990847064d798
thiagomalks <thiagopaula_@outlook.com>
2022-12-20 10:13:28 +0000

    Atualizando versão Zabbix

--- install.sh
+++ install.sh
@@ -29,6 +29,7 @@ service docker enable
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
+##Instalação do PostgreSQL e Zabbix
 #Criando diretório do Zabbix e movendo docker compose
 mkdir -p /home/zabbix && cp docker-compose.yml $_
 
